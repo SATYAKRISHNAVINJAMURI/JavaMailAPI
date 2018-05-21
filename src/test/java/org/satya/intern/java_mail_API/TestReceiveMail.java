c299df9668e45b19b5995237c9174e48ad1edca4
SATYAKRISHNAVINJAMURI <satyakrishnavinjamuri868@gmail.com>
2018-05-22 00:44:12 +0530

    Final Commit for  day 21/05/2018

---
+++ src/test/java/org/satya/intern/java_mail_API/TestReceiveMail.java
@@ -0,0 +1,30 @@
+package org.satya.intern.java_mail_API;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class TestReceiveMail extends TestCase {
+	@Test
+	public void testNoInternetConnection() {
+		
+	}
+	
+	@Test
+	public void testAddressNotFound() {
+		
+		
+	}
+	
+	@Test 
+	public void testHostServerNotReachable() {
+		
+	}
+	
+	@Test
+	public void testAuthenticationFailed() {
+		
+	}
+	
+
+}
